a210938d9f945059ba62a72da6cf0a9efe556800
Patrick Jiang <brojyf@163.com>
2025-04-06 17:34:32 -0700

    Updated init.sql file

--- init.sql
+++ init.sql
@@ -1,41 +1,9 @@
 -- Preparation
-DROP TABLE IF EXISTS Animes;
 DROP TABLE IF EXISTS `Characters`;
+DROP TABLE IF EXISTS Animes;
 DROP TABLE IF EXISTS Genres;
-DROP TABLE IF EXISTS MBTI;
 
-
--- Animes Table
-CREATE TABLE Animes(
-    AnimeID INT PRIMARY KEY,
-    AnimeName VARCHAR(60) UNIQUE NOT NULL,
-    Genre INT,
-    Hulu VARCHAR(200),
-    Crunchyroll VARCHAR(200),
-    Other VARCHAR(200)
-);
-
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/Animes.csv'
-INTO TABLE Animes
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
--- Characters
-CREATE TABLE `Characters`(
-    CharacterID INT PRIMARY KEY,
-    CharacterName VARCHAR(40),
-    MBTI CHAR(4),
-    AnimeID INT
-);
-
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/Characters.csv'
-INTO TABLE `Characters`
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
--- Genres
+-- Genres Table
 CREATE TABLE Genres(
     GenreID INT UNIQUE PRIMARY KEY,
     Genre VARCHAR(20)
@@ -47,15 +15,35 @@ FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
--- MBTI
-CREATE TABLE MBTI(
-    MBTI CHAR(4) UNIQUE,
-    Start INT,
-    End INT
+-- Animes Table
+CREATE TABLE Animes(
+    AnimeID INT PRIMARY KEY,
+    AnimeName VARCHAR(60) UNIQUE NOT NULL,
+    GenreID INT,
+    Rating DECIMAL(3, 1),
+    Hulu VARCHAR(200),
+    Crunchyroll VARCHAR(200),
+    Other VARCHAR(200),
+    FOREIGN KEY (GenreID) REFERENCES Genres(GenreID)
 );
 
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/MBTI.csv'
-INTO TABLE MBTI
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/Animes.csv'
+INTO TABLE Animes
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Characters Table
+CREATE TABLE `Characters`(
+    CharacterID INT PRIMARY KEY,
+    CharacterName VARCHAR(40),
+    MBTI CHAR(4),
+    AnimeID INT,
+    FOREIGN KEY (AnimeID) REFERENCES Animes(AnimeID)
+);
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/Characters.csv'
+INTO TABLE `Characters`
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
