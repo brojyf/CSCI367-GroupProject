c125983200147dc014c6c131b86df15ea0f0e8d1
shiori23nakayama <115039515+shiorinakayama@users.noreply.github.com>
2025-04-24 19:43:37 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import findMatch from "./findMatch.js";
+
+function App() {
+    return (
+        <>
+            <h1>Find Anime Characters Match Your MBTI!</h1>
+            <findMatch />
+        </>
+    );
+}
+
+export default App;
